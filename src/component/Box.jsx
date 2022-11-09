5f0a5d1daa76e47544b74135b786efd16fe545c8
min2oyo <min2u@outlook.com>
2022-11-09 09:07:20 +0900

    Box.jsx CSS 코드정리

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,9 +1,15 @@
 import '../App.css';
 
 const Box = (props) => {
+
+  // CSS
+  const win = { border: "5px solid blue" };
+  const lose = { border: "5px solid red" };
+  const tie = { border: "5px solid black" };
+
   return (
     <>
-      <div className="box">
+      <div className="box" style={props.result === "win" ? win : props.result === "lose" ? lose : tie}>
         <h1>{props.title}</h1 >
         <img className='item-img' src={props.item?.img} />
         <h2>{props.result}</h2>
